0a8da9d8205b034bfd38fbce20a7835a3437e377
AmeerUlAman <w.ameerulaman@gmail.com>
2025-03-11 23:53:33 +0500

    cl cd

--- src/app/hero/hero.tsx
+++ src/app/hero/hero.tsx
@@ -3,16 +3,9 @@ import styles from "./hero.module.css"
 import Nav from "../nav/nav";
 const Hero =()=> {
 return(
-    <div className="">
-{/* <div >
-<Image
-height={371}
-width={3334}
-src={'/logo'}
-alt=""
- className={styles.logocontainer}
-/>
-</div> */}
+    <div className={styles.bg}>
+        <div className={styles.bgc}></div>
+ 
 <Nav/>
 
     </div>
