9278e0c6918deb73423b293c09837a28c3c81c7c
AmeerUlAman <w.ameerulaman@gmail.com>
2025-03-16 03:19:10 +0500

    hero_done

--- src/app/hero/hero.tsx
+++ src/app/hero/hero.tsx
@@ -1,22 +1,43 @@
-import Image from "next/image";
-import styles from "./hero.module.css"
+"use client";
+
+import { useEffect } from "react"; 
+import styles from "./hero.module.css";
 import Nav from "../nav/nav";
-const Hero =()=> {
-return(
+import Typed from "typed.js";
+
+const Hero = () => {
+  useEffect(() => {
+    const typed = new Typed("#typed-text", {
+      strings: [" Applications", " Websites", " Mobile Apps"],
+      typeSpeed: 100,
+      backSpeed: 50,
+      loop: true,
+    });
+
+    return () => typed.destroy(); // Cleanup on unmount
+  }, []);
+
+  return (
     <div className={styles.bg}>
-        <div className={styles.bgc}></div>
- 
-<Nav/>
-<hr />
-<div className={styles.herostmt}>
-<p></p>
-
-</div>
-
+      <Nav />
+ <hr className={styles.h}/>
+      <div className={styles.herostmt}>
+        <div className={styles.herostmtbg}>
+          <p className={styles.heading}>
+            Building software solutions, and innovating AI and automation in <br />
+            <span id="typed-text" className={styles.animatedText}></span>
+          </p>
+          <p className={styles.highlights}>
+            Innovate. Automate. Elevate. – Your Vision, Our Code.
+          </p>
+          <div>
+            <button className={styles.cta}>Get a quote</button>
+            <button className={styles.cta2}>Learn more</button>
+          </div>
+        </div>
+      </div>
     </div>
-
-);
-
-} 
+  );
+};
 
 export default Hero;
