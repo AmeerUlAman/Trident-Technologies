6cdc878b8c0f0c48673ace6a6a10d85435ffb819
AmeerUlAman <w.ameerulaman@gmail.com>
2025-04-01 21:50:22 +0500

    changed a bit

--- src/app/service/services.tsx
+++ src/app/service/services.tsx
@@ -1,14 +1,8 @@
 import styles from "./services.module.css"
 import Servicesdisplay from "./servicesdisplay"
 
-
-interface services {
-name: string,
-im: string,
-description: string,
-
-}
-const Services:React.FC<services> = ({name,im,description}) => {
+ 
+const Services = () => {
     return (
         <div className={styles.whole}>
             <h1 className={styles.title}>What we offer...</h1>
